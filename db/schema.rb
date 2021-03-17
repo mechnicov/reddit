6ba8a50b02fd1f2a18a980cb4aa6a53001f5bf49
Edem Topuzov <keltikan@gmail.com>
2021-03-18 00:54:08 +0300

    Add posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_212655) do
+ActiveRecord::Schema.define(version: 2021_03_17_214649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,21 @@ ActiveRecord::Schema.define(version: 2021_03_17_212655) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.bigint "score_dislikes", default: 0, null: false
+    t.bigint "score_unvoted", default: 0, null: false
+    t.bigint "score_likes", default: 0, null: false
+    t.string "url", null: false
+    t.string "reddit_id", null: false
+    t.integer "comments_count", default: 0, null: false
+    t.datetime "posted_at", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  add_foreign_key "posts", "authors"
 end
