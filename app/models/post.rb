8a2886aab2b8b54ed00ad86bb714d3a478f30d6f
Edem Topuzov <keltikan@gmail.com>
2021-03-18 00:57:11 +0300

    Add Post model

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ApplicationRecord
+  validates :title,
+            :body,
+            :score_dislikes,
+            :score_unvoted,
+            :score_likes,
+            :url,
+            :reddit_id,
+            :comments_count,
+            :posted_at,
+            presence: true
+end
